806a037c2871af5744def1f946b25547684a3a0f
Mahir Hayaat <mahirhayat4u@gmail.com>
2025-11-23 13:30:21 +0530

    Add root route handler

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
-const noteRoutes = require('./routes/noteRoutes');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 // Routes
 app.use('/api/notes', noteRoutes);
 
