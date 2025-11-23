252c5e5a7b538c7340618be24c44bc99a9d472db
Mahir Hayaat <mahirhayat4u@gmail.com>
2025-11-23 13:31:30 +0530

    Fix server crash: restore index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const noteRoutes = require('./routes/noteRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 // Routes
+app.get('/', (req, res) => {
+    res.send('Note App API is running');
+});
+
 app.use('/api/notes', noteRoutes);
 
 // Database Connection
